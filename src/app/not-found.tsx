75d5c81ab172cbd1ef3b99496f6fdcdcd8eb1b54
aarslanzakir <aliarslanzakir@gmail.com>
2026-04-15 17:57:36 +0500

    updated

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>404 - Page Not Found</h2>
+      <p>The page you are looking for does not exist.</p>
+      <Link href="/" style={{ marginTop: "1rem", display: "inline-block" }}>
+        Go back home
+      </Link>
+    </div>
+  );
+}
